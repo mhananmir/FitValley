175361ed01cea005ba6dfcadcac99b290a33da4b
Hanan Mir <mhananm33r@gmail.com>
2023-02-02 13:19:43 -0800

    demo

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "KJWZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLZA",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "5559eeef87mshaf0dd544ccb5deap109fcajsn76954fa0ca37",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
